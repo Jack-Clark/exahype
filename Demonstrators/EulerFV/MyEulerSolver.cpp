481728b3adcc96d037673e60a8bfc988c34972be
tobias <tobias@localhost>
2017-02-13 20:06:32 +0000

    Demonstrators are now available as source code.

---
+++ Demonstrators/EulerFV/MyEulerSolver.cpp
@@ -0,0 +1,106 @@
+/**
+ * This file is part of the ExaHyPE project. For copyright and information
+ * please see www.exahype.eu.
+ */
+#include "MyEulerSolver.h"
+#include "MyEulerSolver_Variables.h"
+#include "Logo.h"
+
+
+
+void EulerFV::MyEulerSolver::init(std::vector<std::string>& cmdlineargs) {
+}
+
+
+bool EulerFV::MyEulerSolver::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, const double t, const double dt) {
+  return tarch::la::equals(t,0.0);
+}
+
+
+void EulerFV::MyEulerSolver::adjustedSolutionValues(const double* const x,
+                                                  const double w,
+                                                  const double t,
+                                                  const double dt, double* Q) {
+  Variables vars(Q);
+  
+  tarch::la::Vector<DIMENSIONS,double> myX( x[0] - 0.06, 1.0-x[1] - 0.25 ); // translate
+  myX *= static_cast<double>(Image.width);
+  tarch::la::Vector<DIMENSIONS,int>    myIntX( 1.2*myX(0) , 1.2*myX(1) );  // scale
+
+  double Energy = 0.1;
+
+  if (
+    myIntX(0) > 0 && myIntX(0) < static_cast<int>(Image.width)
+    &&
+    myIntX(1) > 0 && myIntX(1) < static_cast<int>(Image.height)
+  ) {
+    Energy += 1.0-Image.pixel_data[myIntX(1)*Image.width+myIntX(0)];
+  }
+
+  vars.rho() = 1.0;
+  vars.E()   = Energy;
+  vars.j(0,0,0);
+}
+
+
+exahype::solvers::Solver::RefinementControl EulerFV::MyEulerSolver::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {
+  return exahype::solvers::Solver::RefinementControl::Keep;
+}
+
+
+void EulerFV::MyEulerSolver::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {
+  ReadOnlyVariables vars(Q);
+  Variables eigs(lambda);
+
+  const double GAMMA = 1.4;
+  const double irho = 1./vars.rho();
+  const double p = (GAMMA-1) * (vars.E() - 0.5 * irho * vars.j()*vars.j() );
+
+  double u_n = Q[normalNonZeroIndex + 1] * irho;
+  double c  = std::sqrt(GAMMA * p * irho);
+
+  eigs.rho()=u_n - c;
+  eigs.E()  =u_n + c;
+  eigs.j(u_n,u_n,u_n);
+}
+
+
+void EulerFV::MyEulerSolver::flux(const double* const Q, double** F) {
+  ReadOnlyVariables vars(Q);
+  Fluxes fluxes(F);
+
+  tarch::la::Matrix<3,3,double> I;
+  I = 1, 0, 0,
+      0, 1, 0,
+      0, 0, 1;
+
+  const double GAMMA = 1.4;
+  const double irho = 1./vars.rho();
+  const double p = (GAMMA-1) * (vars.E() - 0.5 * irho * vars.j()*vars.j() );
+
+  fluxes.rho ( vars.j()                                 );
+  fluxes.j   ( irho * outerDot(vars.j(),vars.j()) + p*I );
+  fluxes.E   ( irho * (vars.E() + p) * vars.j()         );
+}
+
+
+void EulerFV::MyEulerSolver::source(const double* const Q, double* S) {
+  Variables source(S);
+  source.rho()=0;
+  source.E()=0;
+  source.j(0,0,0);
+}
+
+
+void EulerFV::MyEulerSolver::boundaryValues(
+    const double* const x,
+    const double t,const double dt,
+    const int faceIndex,
+    const int normalNonZero,
+    const double* const stateInside,
+    double* stateOutside) {
+  ReadOnlyVariables varsInside(stateInside);
+  Variables         varsOutside(stateOutside);
+
+  varsOutside = varsInside;
+}
